599d2be8e185db07d899acaf0fc236f8381cb872
Your Name <nong8033@gmail.com>
2025-03-26 21:59:23 +0700

    code for detection

---
+++ Detect_Blue_Bottles.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import os
+
+def readImage(path):
+    # for file in os.listdir(path):
+    #     if file.endswith(".png, .jpg, .jpeg, .bmp"):
+    #         path_img = os.path.join(path, file)
+    #         img = cv2.imread(path_img)
+    img = cv2.imread(path)
+    img = cv2.resize(img, (1279, 641))
+    return img
+
+def detectionBlueBottles():
+    img = readImage(path)
+    img_copy = img.copy()
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.medianBlur(img, 3)
+
+    upper_blue = np.array([130, 255, 255])
+    lower_blue = np.array([90, 0, 0])
+    
+    canny = cv2.Canny(img_blur, 100, 200)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+    h, w = img.shape
+    # print(h, w)
+
+    mindist = int(min(h, w) / 20)
+    param1 = int(np.mean(canny) * 0.5) * 6.5
+    param2 = int(np.mean(canny) / 2) * 6.5
+    minradius = int(min(h, w) / 40 * 0.5)
+    maxradius = int(min(h, w) / 40 * 0.9)
+
+    count_bottles = []
+  
+    if mask is not None:
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, minDist= mindist, param1= param1, param2=param2, minRadius=minradius, maxRadius=maxradius)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+
+            for point in circles[0]:
+                x, y, r = point
+                cv2.circle(mask, (x ,y), r, (0, 255, 0), 2)
+        
+        blue_bottles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, minDist= mindist, param1= param1, param2=param2, minRadius=minradius, maxRadius=maxradius)
+        blue_bottles = np.uint16(np.around(blue_bottles))
+        
+        for point in blue_bottles[0]:
+            x, y, r = point
+            cv2.circle(img_copy, (x ,y), r, (0, 255, 0), 2)
+
+        for count in blue_bottles[0]:
+            count_bottles.append(count)
+
+    while True:
+        cv2.putText(img_copy, "Total:"+ str(len(count_bottles)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.imshow("IMG", img_copy)
+
+        if cv2.waitKey(0):
+            break
+
+    cv2.destroyAllWindows()
+
+    return count_bottles
+
+
+if __name__ == "__main__":
+    path = 'Data_Bottles (1).png'
+    # path = "Example_Test_Bottles (1).png"
+    detectionBlueBottles()
